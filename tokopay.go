4394a3023420eafd7acd3f27d5ae89be136245a4
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2024-11-23 12:11:48 +0700

    improve robustness of payment gateway integration

--- tokopay.go
+++ tokopay.go
@@ -49,7 +49,7 @@ func createOrderHandler(res http.ResponseWriter, req *http.Request) {
 
 	err := json.NewDecoder(req.Body).Decode(&body)
 	if err != nil {
-		logWithCtx.Error().Err(errors.Wrap(err, "createOrderHandler()")).Msg("invalid json body")
+		logWithCtx.Error().Err(err).Msg("invalid json body")
 		http.Error(res, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
@@ -59,12 +59,7 @@ func createOrderHandler(res http.ResponseWriter, req *http.Request) {
 
 	coupon, err := queries.GetCoupon(ctx, body.CouponCode)
 	if err != nil && errors.Is(err, pgx.ErrNoRows) == false {
-		logWithCtx.
-			Error().
-			Err(errors.Wrap(err, "createOrderHandler()")).
-			Str("coupon_code", body.CouponCode).
-			Msg("failed to get coupon by coupon code")
-
+		logWithCtx.Error().Err(err).Str("coupon_code", body.CouponCode).Msg("failed to get coupon by coupon code")
 		http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
@@ -75,8 +70,8 @@ func createOrderHandler(res http.ResponseWriter, req *http.Request) {
 		body.Amount = int(math.Round(float64(body.Amount) * 0.7))
 	}
 
-	MERCHANT_ID := os.Getenv("Merchant_ID")
-	SECRET_KEY := os.Getenv("Secret_Key")
+	MERCHANT_ID := os.Getenv("MERCHANT_ID")
+	SECRET_KEY := os.Getenv("SECRET_KEY")
 
 	refID := uuid.New()
 	refIDString := refID.String()
@@ -92,8 +87,7 @@ func createOrderHandler(res http.ResponseWriter, req *http.Request) {
 
 	response, err := http.Get(orderURL)
 	if err != nil {
-		errMsg := "failed to make http get request to create order"
-		logWithCtx.Error().Err(errors.Wrap(err, "createOrderHandler()")).Msg(errMsg)
+		logWithCtx.Error().Err(err).Msg("failed to make http get request to create order")
 		http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
@@ -101,7 +95,7 @@ func createOrderHandler(res http.ResponseWriter, req *http.Request) {
 
 	bytes, err := io.ReadAll(response.Body)
 	if err != nil {
-		logWithCtx.Error().Err(errors.Wrap(err, "createOrderHandler()")).Msg("failed to read order response")
+		logWithCtx.Error().Err(err).Str("order_id", refIDString).Msg("failed to read tokopay order response")
 		http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
@@ -111,7 +105,7 @@ func createOrderHandler(res http.ResponseWriter, req *http.Request) {
 	}
 
 	if err := json.Unmarshal(bytes, &responseStatus); err != nil {
-		logWithCtx.Error().Err(errors.Wrap(err, "createOrderHandler()")).Msg("failed to unmarshal order response status")
+		logWithCtx.Error().Err(err).Str("order_id", refIDString).Msg("failed to unmarshal tokopay order response status")
 		http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
@@ -120,13 +114,7 @@ func createOrderHandler(res http.ResponseWriter, req *http.Request) {
 	case string:
 		var orderSuccess OrderSuccess
 		if err := json.Unmarshal(bytes, &orderSuccess); err != nil {
-			logWithCtx.
-				Error().
-				Err(errors.Wrap(err, "createOrderHandler()")).
-				Str("order_id", refIDString).
-				Str("transaction_id", orderSuccess.Data.TrxID).
-				Msg("failed to unmarshal successful order")
-
+			logWithCtx.Error().Err(err).Str("order_id", refIDString).Msg("failed to unmarshal tokopay successful order")
 			http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 			return
 		}
@@ -136,11 +124,17 @@ func createOrderHandler(res http.ResponseWriter, req *http.Request) {
 			Str("order_id", refIDString).
 			Str("transaction_id", orderSuccess.Data.TrxID).
 			Int("amount", body.Amount).
-			Msg("successfully created order")
+			Msg("successfully created tokopay order")
 
 		tx, err := db.Begin(ctx)
 		if err != nil {
-			logWithCtx.Error().Err(errors.Wrap(err, "createOrderHandler()")).Msg("failed to start db transaction")
+			logWithCtx.
+				Error().
+				Err(err).
+				Str("order_id", refIDString).
+				Str("coupon_code", body.CouponCode).
+				Msg("failed to start db transaction to create order and/or decrement coupon quota")
+
 			http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 			return
 		}
@@ -163,26 +157,16 @@ func createOrderHandler(res http.ResponseWriter, req *http.Request) {
 		)
 
 		if err != nil {
-			logWithCtx.
-				Error().
-				Err(errors.Wrap(err, "createOrderHandler()")).
-				Str("order_id", refIDString).
-				Msg("failed to insert order to database")
-
+			logWithCtx.Error().Err(err).Str("order_id", refIDString).Msg("failed to create order")
 			http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 			return
 		}
-		logWithCtx.Info().Str("order_id", refIDString).Msg("successfully inserted order to database")
+		logWithCtx.Info().Str("order_id", refIDString).Msg("successfully createed order")
 
 		if couponCode.Valid {
 			err = qtx.DecrementCouponQuota(ctx, couponCode.String)
 			if err != nil {
-				logWithCtx.
-					Error().
-					Err(errors.Wrap(err, "createOrderHandler()")).
-					Str("coupon_code", body.CouponCode).
-					Msg("failed to decrement coupon quota")
-
+				logWithCtx.Error().Err(err).Str("coupon_code", body.CouponCode).Msg("failed to decrement coupon quota")
 				http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 				return
 			}
@@ -191,38 +175,51 @@ func createOrderHandler(res http.ResponseWriter, req *http.Request) {
 
 		err = tx.Commit(ctx)
 		if err != nil {
-			logWithCtx.Error().Err(errors.Wrap(err, "webhookHandler()")).Msg("failed to commit transaction")
+			logWithCtx.
+				Error().
+				Err(err).
+				Str("order_id", refIDString).
+				Str("coupon_code", body.CouponCode).
+				Msg("failed to commit transaction to create order and/or decrement coupon quota")
+
 			http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 			return
 		}
 
-		err = json.NewEncoder(res).Encode(struct {
+		log.
+			Info().
+			Str("order_id", refIDString).
+			Str("coupon_code", body.CouponCode).
+			Msg("successfully created order and/or decremented coupon quota")
+
+		respBody := struct {
 			QRLink string `json:"qr_link"`
-		}{QRLink: orderSuccess.Data.QrLink})
-
-		if err != nil {
-			logWithCtx.Error().Err(errors.Wrap(err, "createOrderHandler()")).Msg("failed to encode successful order to json")
-			http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			return
+		}{
+			QRLink: orderSuccess.Data.QrLink,
 		}
 
-		res.Header().Set("Content-Type", "application/json")
-		res.WriteHeader(http.StatusCreated)
+		err = sendJSONSuccessResponse(res, SuccessResponseParams{StatusCode: http.StatusCreated, Data: respBody})
+		if err != nil {
+			logWithCtx.Error().Err(err).Msg("failed to send json success response")
+			http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
 
 	case float64:
 		var orderError OrderError
 		if err := json.Unmarshal(bytes, &orderError); err != nil {
-			logWithCtx.Error().Err(errors.Wrap(err, "createOrderHandler()")).Msg("failed to unmarshal failed order")
+			logWithCtx.Error().Err(err).Str("order_id", refIDString).Msg("failed to unmarshal tokopay failed order")
 		} else {
-			logWithCtx.Error().Err(errors.New("createOrderHandler(): " + orderError.ErrorMsg)).Msg("")
+			logWithCtx.Error().Err(errors.New(orderError.ErrorMsg)).Str("order_id", refIDString).Msg("")
 		}
 		http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 
 	default:
 		logWithCtx.
 			Error().
-			Err(errors.Wrap(err, "createOrderHandler()")).
-			Msgf("unexpected order response status type: %T", responseStatus)
+			Err(err).
+			Str("order_id", refIDString).
+			Str("order_payload", string(bytes)).
+			Msgf("unknown tokopay order response status type: %T", responseStatus.Status)
 
 		http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 	}
@@ -248,114 +245,143 @@ type TokopayWebhook struct {
 
 func webhookHandler(res http.ResponseWriter, req *http.Request) {
 	logWithCtx := log.Ctx(req.Context()).With().Logger()
-	var body TokopayWebhook
-
-	err := json.NewDecoder(req.Body).Decode(&body)
+	bytes, err := io.ReadAll(req.Body)
 	if err != nil {
-		logWithCtx.Error().Err(errors.Wrap(err, "webhookHandler()")).Msg("invalid json body")
-		http.Error(res, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		return
-	}
-
-	refIDBytes, err := uuid.Parse(body.ReffID)
-	if err != nil {
-		logWithCtx.Error().Err(errors.Wrap(err, "webhookHandler()")).Msg("failed to parse uuid string to uuid bytes")
+		logWithCtx.Error().Err(err).Msg("failed to read tokopay webhook request")
 		http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 
-	ctx := context.Background()
-	tx, err := db.Begin(ctx)
-	if err != nil {
-		logWithCtx.Error().Err(errors.Wrap(err, "webhookHandler()")).Msg("failed to start db transaction")
+	var requestStatus struct {
+		Status interface{} `json:"status"`
+	}
+
+	if err := json.Unmarshal(bytes, &requestStatus); err != nil {
+		logWithCtx.Error().Err(err).Msg("failed to unmarshal tokopay webhook request status")
 		http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
-	defer tx.Rollback(ctx)
 
-	qtx := queries.WithTx(tx)
-	orderWithUser, err := qtx.GetOrderByIDWithUser(ctx, pgtype.UUID{Bytes: refIDBytes, Valid: true})
-	if err != nil {
+	switch requestStatus.Status.(type) {
+	case string:
+		if requestStatus.Status != "Success" && requestStatus.Status != "Completed" {
+			logWithCtx.
+				Error().
+				Err(err).
+				Str("webhook_payload", string(bytes)).
+				Msgf("unknown tokopay webhook request status value: %s", requestStatus.Status)
+
+			http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		var body TokopayWebhook
+		if err := json.Unmarshal(bytes, &body); err != nil {
+			logWithCtx.Error().Err(err).Msg("failed to unmarshal tokopay webhook request")
+			http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		refIDBytes, err := uuid.Parse(body.ReffID)
+		if err != nil {
+			logWithCtx.Error().Err(err).Msg("failed to parse uuid string to uuid bytes")
+			http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		ctx := context.Background()
+		orderWithUser, err := queries.GetOrderByIDWithUser(ctx, pgtype.UUID{Bytes: refIDBytes, Valid: true})
+		if err != nil {
+			logWithCtx.Error().Err(err).Str("order_id", body.ReffID).Msg("failed to get order with user by order id")
+			http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		tx, err := db.Begin(ctx)
+		if err != nil {
+			logWithCtx.
+				Error().
+				Err(err).
+				Str("order_id", body.ReffID).
+				Str("user_id", orderWithUser.UserID).
+				Msg("failed to start db transaction to update order status and user subscription")
+
+			http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		defer tx.Rollback(ctx)
+
+		qtx := queries.WithTx(tx)
+		err = qtx.UpdateOrderStatus(
+			ctx,
+			repository.UpdateOrderStatusParams{
+				ID:            orderWithUser.OrderID,
+				PaymentStatus: repository.PaymentStatusSuccess,
+			},
+		)
+
+		if err != nil {
+			logWithCtx.Error().Err(err).Str("order_id", body.ReffID).Msg("failed to update order status")
+			http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		upgradedAt := time.Now()
+		expiredAt := time.Unix(upgradedAt.Unix()+int64(orderWithUser.SubscriptionDuration), 0)
+		err = qtx.UpdateUserSubscription(
+			ctx,
+			repository.UpdateUserSubscriptionParams{
+				ID:          orderWithUser.UserID,
+				AccountType: repository.AccountTypePremium,
+				UpgradedAt:  pgtype.Timestamptz{Time: upgradedAt, Valid: true},
+				ExpiredAt:   pgtype.Timestamptz{Time: expiredAt, Valid: true},
+			},
+		)
+
+		if err != nil {
+			logWithCtx.Error().Err(err).Str("user_id", orderWithUser.UserID).Msg("failed to update user subscription")
+			http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		err = tx.Commit(ctx)
+		if err != nil {
+			logWithCtx.
+				Error().
+				Err(err).
+				Str("order_id", body.ReffID).
+				Str("user_id", orderWithUser.UserID).
+				Msg("failed to commit transaction to update order status and user subscription")
+
+			http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
 		logWithCtx.
-			Error().
-			Err(errors.Wrap(err, "webhookHandler()")).
+			Info().
 			Str("order_id", body.ReffID).
-			Msg("failed to get order with user by order id")
-
-		http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-
-	err = qtx.UpdateOrderStatus(
-		ctx,
-		repository.UpdateOrderStatusParams{
-			ID:            orderWithUser.OrderID,
-			PaymentStatus: repository.PaymentStatusSuccess,
-		},
-	)
-
-	if err != nil {
-		logWithCtx.
-			Error().
-			Err(errors.Wrap(err, "webhookHandler()")).
-			Str("order_id", body.ReffID).
-			Msg("failed to update order status")
-
-		http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-
-	upgradedAt := time.Now()
-	expiredAt := time.Unix(upgradedAt.Unix()+int64(orderWithUser.SubscriptionDuration), 0)
-	err = qtx.UpdateUserSubscription(
-		ctx,
-		repository.UpdateUserSubscriptionParams{
-			ID:          orderWithUser.UserID,
-			AccountType: repository.AccountTypePremium,
-			UpgradedAt:  pgtype.Timestamptz{Time: upgradedAt, Valid: true},
-			ExpiredAt:   pgtype.Timestamptz{Time: expiredAt, Valid: true},
-		},
-	)
-
-	if err != nil {
-		logWithCtx.
-			Error().
-			Err(errors.Wrap(err, "webhookHandler()")).
 			Str("user_id", orderWithUser.UserID).
-			Msg("failed to update user subscription")
+			Msg("successfully updated order status and user subscription")
 
-		http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
+		respBody := struct {
+			Status bool `json:"status"`
+		}{
+			Status: true,
+		}
 
-	err = tx.Commit(ctx)
-	if err != nil {
+		err = sendJSONSuccessResponse(res, SuccessResponseParams{StatusCode: http.StatusOK, Data: respBody})
+		if err != nil {
+			logWithCtx.Error().Err(err).Msg("failed to send json success response")
+			http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
+
+	default:
 		logWithCtx.
 			Error().
-			Err(errors.Wrap(err, "webhookHandler()")).
-			Str("order_id", body.ReffID).
-			Str("user_id", orderWithUser.UserID).
-			Msg("failed to commit transaction to update order status and user subscription")
+			Err(err).
+			Str("webhook_payload", string(bytes)).
+			Msgf("unknown tokopay webhook request status type: %T", requestStatus.Status)
 
 		http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
 	}
-
-	logWithCtx.
-		Info().
-		Str("order_id", body.ReffID).
-		Str("user_id", orderWithUser.UserID).
-		Msg("successfully updated order status and user subscription")
-
-	err = json.NewEncoder(res).Encode(struct {
-		Status bool `json:"status"`
-	}{Status: true})
-
-	if err != nil {
-		logWithCtx.Error().Err(errors.Wrap(err, "webhookHandler()")).Msg("failed to encode webhook response")
-		http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-
-	res.Header().Set("Content-Type", "application/json")
 }
