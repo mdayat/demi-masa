0d1b538b8c30d955491b77a04ccfbb76de8b9cfe
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2024-12-16 21:38:18 +0700

    create some utils related to prayer

--- internal/prayer/utils.go
+++ internal/prayer/utils.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/mdayat/demi-masa-be/internal/services"
 	"github.com/mdayat/demi-masa-be/repository"
@@ -15,6 +16,10 @@ func MakePrayerCalendarKey(timeZone repository.IndonesiaTimeZone) string {
 
 func MakeLastDayPrayerKey(timeZone repository.IndonesiaTimeZone) string {
 	return fmt.Sprintf("prayer:last_day:%s", timeZone)
+}
+
+func MakePenultimateDayPrayerKey(timeZone repository.IndonesiaTimeZone) string {
+	return fmt.Sprintf("prayer:penultimate_day:%s", timeZone)
 }
 
 func GetPrayerCalendar(ctx context.Context, timeZone repository.IndonesiaTimeZone) (PrayerCalendar, error) {
@@ -45,4 +50,31 @@ func GetLastDayPrayer(ctx context.Context, timeZone repository.IndonesiaTimeZone
 	}
 
 	return lastDayPrayer, nil
+}
+
+func GetPenultimateDayPrayer(ctx context.Context, timeZone repository.IndonesiaTimeZone) (Prayers, error) {
+	penultimateDayPrayerJSON, err := services.GetRedis().Get(ctx, MakeLastDayPrayerKey(timeZone)).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	var penultimateDayPrayer Prayers
+	err = json.Unmarshal([]byte(penultimateDayPrayerJSON), &penultimateDayPrayer)
+	if err != nil {
+		return nil, err
+	}
+
+	return penultimateDayPrayer, nil
+}
+
+func IsLastDay(currentTime *time.Time) bool {
+	firstDayNextMonth := time.Date(currentTime.Year(), currentTime.Month()+1, 1, 0, 0, 0, 0, currentTime.Location())
+	lastDayCurrentMonth := firstDayNextMonth.AddDate(0, 0, -1)
+	return currentTime.Day() == lastDayCurrentMonth.Day()
+}
+
+func IsPenultimateDay(currentTime *time.Time) bool {
+	firstDayNextMonth := time.Date(currentTime.Year(), currentTime.Month()+1, 1, 0, 0, 0, 0, currentTime.Location())
+	penultimateDayCurrentMonth := firstDayNextMonth.AddDate(0, 0, -2)
+	return currentTime.Day() == penultimateDayCurrentMonth.Day()
 }
