8d25e90b6d710b136ab5116d8411e76962f3029a
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2024-12-25 06:29:13 +0700

    fix wrong shutdown command

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Stopping worker service..."
-docker-compose stop worker
+docker compose stop worker
 
 sleep 3
 
 echo "Stopping all services..."
-docker-compose down
+docker compose down
